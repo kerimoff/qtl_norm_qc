e446dc27ffd1bc4c6d94019126621df90228af90
Kaur Alasoo <kaur.alasoo@gmail.com>
2019-06-26 00:32:48 +0300

    Check that the MBV file path is not empty

--- feature_counts_qc.R
+++ feature_counts_qc.R
@@ -121,22 +121,22 @@ if (is.null(study_name)) {
   study_name <- sample_metadata$study[1] 
 }
 
-message("## Perform PCA calculation ##")
+# message("## Perform PCA calculation ##")
 pca_res <- eQTLUtils::plotPCAAnalysis(study_data_se = se, export_output = generate_plots, html_output = build_html, output_dir = output_dir)
 saveRDS(pca_res, paste0(output_dir, paste0("/rds/", study_name ,"_pca_res.rds")))
 readr::write_tsv(pca_res$pca_matrix, paste0(output_dir, paste0("/tsv/", study_name ,"_pca_matrix.tsv")))
 
-message("## Perform MDS calculation ##")
+# message("## Perform MDS calculation ##")
 mds_res <- eQTLUtils::plotMDSAnalysis(study_data_se = se, export_output = generate_plots, html_output = build_html, output_dir = output_dir)
 saveRDS(mds_res, paste0(output_dir, paste0("/rds/", study_name ,"_mds_res.rds")))
 readr::write_tsv(mds_res, paste0(output_dir, paste0("/tsv/", study_name ,"_mds_matrix.tsv")))
 
-message("## Perform sex-specific gene expression analysis ##")
+# message("## Perform sex-specific gene expression analysis ##")
 sex_spec_gene_exp <- eQTLUtils::plotSexQC(study_data = se, export_output = generate_plots, html_output = build_html, output_dir = output_dir)
 saveRDS(sex_spec_gene_exp, paste0(output_dir, paste0("/rds/", study_name ,"_sex_spec_gene_exp_res.rds")))
 readr::write_tsv(sex_spec_gene_exp, paste0(output_dir, paste0("/tsv/", study_name ,"_sex_spec_gene_exp_matrix.tsv")))
 
-message("## Caclulate median TPM in each biological context ##")
+# message("## Caclulate median TPM in each biological context ##")
 median_tpm_df = eQTLUtils::estimateMedianTPM(se, subset_by = "qtl_group", assay_name = "counts")
 gzfile = gzfile(paste0(output_dir, paste0("/median_tpm/", study_name ,"_median_tpm.tsv.gz")), "w")
 write.table(median_tpm_df, gzfile, sep = "\t", row.names = F, quote = F)
@@ -145,6 +145,10 @@ close(gzfile)
 if (!is.null(mbv_files_dir)) {
   message("## Perform MBV Analysis ##")
   mbv_results = eQTLUtils::mbvImportData(mbv_dir = mbv_files_dir, suffix = ".mbv_output.txt")
+  if(length(mbv_results) == 0){
+    stop("No MBV files found in --mbvdir")
+  }
+  
   best_matches = purrr::map_df(mbv_results, eQTLUtils::mbvFindBestMatch, .id = "sample_id") %>% dplyr::arrange(distance)
   mbv_meta = SummarizedExperiment::colData(se) %>% as.data.frame() %>% dplyr::as_tibble() %>% dplyr::select(sample_id, genotype_id)
   best_matches <- dplyr::left_join(mbv_meta, best_matches, by = "sample_id")
@@ -156,21 +160,5 @@ if (!is.null(mbv_files_dir)) {
   }
 }
 message("## RNA Quality Control is completed! ##")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
