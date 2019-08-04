765fbf78a022a0a51147a235a8f454dcd2714122
kerimoff <kerimov.nurlan@gmail.com>
2019-08-04 20:21:40 +0300

    add automative script for all quants

--- norm_the_study.sh
+++ norm_the_study.sh
@@ -11,6 +11,11 @@ else
     echo "Input path is: "$1
 fi
 
+# Check if input directoru exists
+if [ ! -d "$1" ]; then
+  echo "Input path does not exist! "$1
+  exit 1
+fi
 
 if [ "$2" == "" ]; then
     echo "Sample metadata file is not provided"
@@ -19,7 +24,7 @@ else
 fi
 
 # Check if input directoru exists
-if [ ! -d "$2" ]; then
+if [ ! -f "$2" ]; then
   echo $2" : Sample metadata file does not exist!"
   exit 1
 fi
@@ -31,13 +36,13 @@ else
     echo "Output path is: "$3
 fi
 
-# Check if input directoru exists
-if [ ! -d "$1" ]; then
-  echo "Input path does not exist!"
-  exit 1
-fi
+
 
 echo "Starting normalisation of "$1 
+echo ""
+echo ""
+echo "Start gene count normalisation"
+echo ""
 
 Rscript normaliseCountMatrix.R\
  -c $1/featureCounts/merged_gene_counts.txt\
@@ -45,20 +50,37 @@ Rscript normaliseCountMatrix.R\
  -p /gpfs/hpc/home/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/gene_counts_Ensembl_96_phenotype_metadata.tsv.gz\
  -o $3
 
-# Rscript normaliseCountMatrix.R\
-#  -c /gpfs/hpc/home/kerimov/rnaseq/results_Alasoo_2018/dexseq_exon_counts/merged_exon_counts.tsv\
-#  -s data/sample_metadata/Alasoo_2018.tsv\
-#  -p /gpfs/hpc/home/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/exon_counts_Ensembl_96_phenotype_metadata.tsv.gz\
-#  -q exon_counts
+echo ""
+echo "Start exon count normalisation"
+echo ""
 
-# Rscript normaliseCountMatrix.R\
-#  -c /gpfs/hpc/home/kerimov/rnaseq/results_Alasoo_2018/Salmon/merged_counts/gencode.v30.transcripts/gencode.v30.transcripts_TPM_merged.txt\
-#  -s data/sample_metadata/Alasoo_2018.tsv\
-#  -p /gpfs/hpc/home/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/transcript_usage_Ensembl_96_phenotype_metadata.tsv.gz\
-#  -q transcript_usage
+Rscript normaliseCountMatrix.R\
+ -c $1/dexseq_exon_counts/merged_exon_counts.tsv\
+ -s $2\
+ -p /gpfs/hpc/home/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/exon_counts_Ensembl_96_phenotype_metadata.tsv.gz\
+ -o $3\
+ -q exon_counts
 
-# Rscript normaliseCountMatrix.R\
-#  -c /gpfs/hpc/home/kerimov/rnaseq/results_Alasoo_2018/Salmon/merged_counts/txrevise_merged_counts\
-#  -s data/sample_metadata/Alasoo_2018.tsv\
-#  -p /gpfs/hpc/home/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/txrevise_Ensembl_96_phenotype_metadata.tsv.gz\
-#  -q txrevise
+echo ""
+echo "Start transcript count normalisation"
+echo ""
+
+Rscript normaliseCountMatrix.R\
+ -c $1/Salmon/merged_counts/TPM/gencode.v30.transcripts.TPM.merged.txt\
+ -s $2\
+ -p /gpfs/hpc/home/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/transcript_usage_Ensembl_96_phenotype_metadata.tsv.gz\
+ -o $3\
+ -q transcript_usage\
+
+
+echo ""
+echo "Start txrevise count normalisation"
+echo ""
+
+Rscript normaliseCountMatrix.R\
+ -c $1/Salmon/merged_counts/TPM/\
+ -s $2\
+ -p /gpfs/hpc/home/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/txrevise_Ensembl_96_phenotype_metadata.tsv.gz\
+ -o $3\
+ -q txrevise
+
