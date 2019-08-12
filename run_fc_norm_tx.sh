323bf19eec5ffa0ba28a51084e4644569b300a9f
Kaur Alasoo <kaur.alasoo@gmail.com>
2019-08-12 22:39:58 +0300

    Remove redundant examples

--- run_fc_norm_tx.sh
+++
@@ -1,5 +0,0 @@
-Rscript normaliseCountMatrix.R\
- -c /gpfs/hpc/home/kerimov/rnaseq/results_Alasoo_2018/Salmon/merged_counts/gencode.v30.transcripts/gencode.v30.transcripts_TPM_merged.txt\
- -s data/sample_metadata/Alasoo_2018.tsv\
- -p /gpfs/hpc/home/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/transcript_usage_Ensembl_96_phenotype_metadata.tsv.gz\
- -q transcript_usage
