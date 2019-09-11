9e9a5b61303794adc6b320322c4f6380d4a4669f
kerimoff <kerimov.nurlan@gmail.com>
2019-09-11 20:39:49 +0300

    update .gitignore

---
+++ Schmiedel_exon_norm.sh
@@ -0,0 +1,11 @@
+Rscript normaliseCountMatrix.R\
+ -c /gpfs/hpc/home/a72094/datasets/processed/Schmiedel_2018/dexseq_exon_counts/merged_exon_counts.tsv\
+ -s /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Schmiedel_2018.tsv\
+ -p /gpfs/hpc/home/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/exon_counts_Ensembl_96_phenotype_metadata.tsv.gz\
+ -o /gpfs/hpc/home/kerimov/featurecounts_qc/results_Schmiedel_2018_exon/\
+ -q exon_counts\
+ --eqtlutils /gpfs/hpc/home/a72094/projects/eQTLUtils\
+ --filter_qc TRUE\
+
+ 
+ 
---
+++ normalise_all_HipSci.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# HipSci
+singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/HipSci /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/HipSci.tsv /gpfs/hpc/home/kerimov/featurecounts_qc/new_results_12Aug/Hipsci/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
