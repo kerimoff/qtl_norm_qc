8a6fc2d102759d6c57ce77aba23780a476fbc069
kerimoff <kerimov.nurlan@gmail.com>
2019-09-11 20:35:16 +0300

    normalise script of BrainSeq and ROSMAP

--- normalise_all_studies.sh
+++ normalise_all_studies.sh
@@ -1,35 +1,51 @@
 #!/bin/bash
 
 # HipSci
-singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/HipSci /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/HipSci.tsv /gpfs/hpchome/a72094/datasets/processed/expression_matrices/Hipsci/
+# singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/HipSci /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/HipSci.tsv /gpfs/hpc/home/kerimov/featurecounts_qc/new_results_12Aug/Hipsci/
 
 # Alasoo_2018
-singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/Alasoo_2018 /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Alasoo_2018.tsv /gpfs/hpchome/a72094/datasets/processed/expression_matrices/Alasoo_2018/
+# singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/Alasoo_2018 /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Alasoo_2018.tsv /gpfs/hpc/home/kerimov/featurecounts_qc/new_results_12Aug/Alasoo_2018/
 
 # GENCORD
-singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/GENCORD /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/GENCORD.tsv /gpfs/hpchome/a72094/datasets/processed/expression_matrices/GENCORD/
+# singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/GENCORD /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/GENCORD.tsv /gpfs/hpc/home/kerimov/featurecounts_qc/new_results_12Aug/GENCORD/
 
 # GEUVADIS
-singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/GEUVADIS /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/GEUVADIS.tsv /gpfs/hpchome/a72094/datasets/processed/expression_matrices/GEUVADIS/
+# singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/GEUVADIS /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/GEUVADIS.tsv /gpfs/hpc/home/kerimov/featurecounts_qc/new_results_12Aug/GEUVADIS/
 
 # Lepik_2017
-singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/Lepik_2017 /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Lepik_2017.tsv /gpfs/hpchome/a72094/datasets/processed/expression_matrices/Lepik_2017/
+# singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/Lepik_2017 /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Lepik_2017.tsv /gpfs/hpc/home/kerimov/featurecounts_qc/new_results_12Aug/Lepik_2017/
 
 # Nedelec_2016
-singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/Nedelec_2016 /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Nedelec_2016.tsv /gpfs/hpchome/a72094/datasets/processed/expression_matrices/Nedelec_2016/
+# singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/Nedelec_2016 /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Nedelec_2016.tsv /gpfs/hpc/home/kerimov/featurecounts_qc/new_results_12Aug/Nedelec_2016/
 
 # Quach_2016
-singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/Quach_2016 /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Quach_2016.tsv /gpfs/hpchome/a72094/datasets/processed/expression_matrices/Quach_2016/
+# singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/Quach_2016 /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Quach_2016.tsv /gpfs/hpc/home/kerimov/featurecounts_qc/new_results_12Aug/Quach_2016/
 
 # Schmiedel_2018
-singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/Schmiedel_2018 /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Schmiedel_2018.tsv /gpfs/hpchome/a72094/datasets/processed/expression_matrices/Schmiedel_2018/
+# singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/Schmiedel_2018 /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Schmiedel_2018.tsv /gpfs/hpc/home/kerimov/featurecounts_qc/new_results_12Aug/Schmiedel_2018/
 
 # Schwartzentruber_2018
-singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/Schwartzentruber_2018 /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Schwartzentruber_2018.tsv /gpfs/hpchome/a72094/datasets/processed/expression_matrices/Schwartzentruber_2018/
+# singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/Schwartzentruber_2018 /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/Schwartzentruber_2018.tsv /gpfs/hpc/home/kerimov/featurecounts_qc/new_results_12Aug/Schwartzentruber_2018/
 
 # TwinsUK
-singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/TwinsUK /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/TwinsUK.tsv /gpfs/hpchome/a72094/datasets/processed/expression_matrices/TwinsUK/
+# singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/TwinsUK /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/TwinsUK.tsv /gpfs/hpc/home/kerimov/featurecounts_qc/new_results_12Aug/TwinsUK/
 
 # van_de_Bunt_2015
-singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/van_de_Bunt_2015 /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/van_de_Bunt_2015.tsv /gpfs/hpchome/a72094/datasets/processed/expression_matrices/van_de_Bunt_2015/
+# singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/van_de_Bunt_2015 /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/van_de_Bunt_2015.tsv /gpfs/hpc/home/kerimov/featurecounts_qc/new_results_12Aug/van_de_Bunt_2015/
+
+# BLUEPRINT_PE
+# singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/BLUEPRINT_PE /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/BLUEPRINT_PE.tsv /gpfs/hpc/home/kerimov/featurecounts_qc/results_BLUEPRINT/BLUEPRINT_PE/
+
+# BLUEPRINT_SE
+# singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/BLUEPRINT_SE /gpfs/hpc/home/a72094/datasets/controlled_access/SampleArcheology/studies/cleaned/BLUEPRINT_SE.tsv /gpfs/hpc/home/kerimov/featurecounts_qc/results_BLUEPRINT/BLUEPRINT_SE/
+
+# ROSMAP
+singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/ROSMAP /gpfs/hpc/home/kerimov/SampleArcheology/studies/cleaned/ROSMAP.tsv /gpfs/hpc/home/kerimov/featurecounts_qc/results/results_ROSMAP/
+
+# BrainSeq
+singularity exec -B /gpfs/:/gpfs/ docker://kerimoff/eqtlutils bash norm_the_study.sh /gpfs/hpc/home/a72094/datasets/processed/BrainSeq /gpfs/hpc/home/kerimov/SampleArcheology/studies/cleaned/BrainSeq.tsv /gpfs/hpc/home/kerimov/featurecounts_qc/results/results_BrainSeq/
+
+
+
+
 
