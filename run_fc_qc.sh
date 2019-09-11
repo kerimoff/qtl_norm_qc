f21a9980b0cff23dab9e9c3ce8d882b198427b92
kerimoff <kerimov.nurlan@gmail.com>
2019-09-11 20:37:59 +0300

    add filter_qc option

--- run_fc_qc.sh
+++ run_fc_qc.sh
@@ -1,7 +1,8 @@
 Rscript feature_counts_qc.R\
- -c data/counts/Alasoo_test_data/Alasoo_merged_gene_counts.txt\
- -s data/sample_metadata/Alasoo_2018.tsv\
- -p data/annotations/gene_counts_Ensembl_96_phenotype_metadata.tsv.gz\
- -o ./results/RNA_QC_script_results_Alasoo\
- -m ./mbv\
- --build_html TRUE
+ -c /gpfs/hpc/home/a72094/datasets/processed/BrainSeq/featureCounts/merged_gene_counts.txt\
+ -s /gpfs/hpc/home/kerimov/SampleArcheology/studies/cleaned/BrainSeq.tsv\
+ -p /gpfs/hpc/home/a72094/annotations/eQTLCatalogue/v0.1/phenotype_metadata/gene_counts_Ensembl_96_phenotype_metadata.tsv.gz\
+ -o ./results/RNA_QC_script_results_BrainSeq\
+ --eqtlutils /gpfs/hpc/home/kerimov/eQTLUtils\
+ --filter_qc TRUE
+
